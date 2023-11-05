55afd8809550b2327c8764e861bbc879ba572043
Mariyaay <mariya.y2802@gmail.com>
2023-11-05 17:25:43 +0200

    Upload code

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Please enter your name:");
+
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
+        System.out.println("Please enter your hobby:");
+        String hobby = scanner.nextLine();
+
+
+    }
+}
